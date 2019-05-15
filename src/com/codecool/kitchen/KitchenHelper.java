bd25547f96aa46951f0b65dbf5b803bdd6c69d6e
Szedlacsek Lilla <szedlacsek.lilla@gmail.com>
2019-05-15 09:15:09 +0200

    Created enumMap for KitchenHelpers to keep track of their ingredients

--- src/com/codecool/kitchen/KitchenHelper.java
+++ src/com/codecool/kitchen/KitchenHelper.java
@@ -1,14 +1,23 @@
 package com.codecool.kitchen;
 
 import java.util.Date;
+import java.util.EnumMap;
 
 public class KitchenHelper extends Employee {
 
-    public KitchenHelper(){}
+    public KitchenHelper() {
+    }
 
     public KitchenHelper(String name, Date birthday, double salary) {
         this.name = name;
         this.birthday = birthday;
         this.salary = salary;
+
+
+        EnumMap<Ingredient, Integer> ingredientMap = new EnumMap<>(Ingredient.class);
+        ingredientMap.put(Ingredient.CARROT, 0);
+        ingredientMap.put(Ingredient.MEAT, 0);
+        ingredientMap.put(Ingredient.POTATO, 0);
     }
+
 }
