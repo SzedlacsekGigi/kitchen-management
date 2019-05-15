c58add20671f47a78f4ff51290adaa2fffae3449
Szedlacsek Lilla <szedlacsek.lilla@gmail.com>
2019-05-15 09:05:47 +0200

    Added boolean value to keep track of if FoddPreparer subclasses have their knife

--- src/com/codecool/kitchen/FoodPreparer.java
+++ src/com/codecool/kitchen/FoodPreparer.java
@@ -2,5 +2,11 @@ package com.codecool.kitchen;
 
 public abstract class FoodPreparer extends Employee {
 
+    private boolean hasKnife = false;
 
+    public abstract void cook();
+
+    public void setHasKnife(boolean hasKnife) {
+        this.hasKnife = hasKnife;
+    }
 }
