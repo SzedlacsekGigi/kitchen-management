76134bba3ff20c0d542d7073b57d353cae0bf851
Szedlacsek Lilla <szedlacsek.lilla@gmail.com>
2019-05-16 11:18:32 +0200

    Created main method

--- src/com/codecool/kitchen/Main.java
+++ src/com/codecool/kitchen/Main.java
@@ -3,6 +3,8 @@ package com.codecool.kitchen;
 public class Main {
 
     public static void main (String[] args) {
-        System.out.println("I'm the main class");
+        Restaurant restaurant = new Restaurant();
+        restaurant.hireEmployees();
+        restaurant.simulateDay();
     }
 }
